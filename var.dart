0f03c4346b1f2636c1174d91f40cfc8bd822b20f
旧城以西 <86241606+jiuchengyixiaicoding@users.noreply.github.com>
2025-05-22 17:50:09 +0800

    Create var.dart

---
+++ var.dart
@@ -0,0 +1,11 @@
+/**
+ * 虽然 Dart 是 代码类型安全 的语言，你仍然可以用 var 来定义变量，而不用显式指定它们的类型。由于其支持类型推断，因此大多数变量的类型会由它们的初始化内容决定：
+ */
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg',
+};
